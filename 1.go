87de8b1d96f433ec6fd0882b7386a8eb01b296fd
Kishan <kishanrajeevi.sangamone@gmail.com>
2024-10-01 16:28:43 +0400

    Did some basic coding

---
+++ 1.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"fmt" 
+	"time"
+)
+
+func main(){
+	num1 := 10 
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	fmt.Print(num1)
+	time.Sleep(10 * time.Second)
+	fmt.Print("end")
+}
